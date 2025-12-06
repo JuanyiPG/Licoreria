a3e5221a5f6f50cbdf72e588c2ff2432dcbe91d7
JuanyiPG <jspinto57@soy.sena.edu.co>
2025-12-06 13:40:59 -0500

    add all document

---
+++ proyecto-java/AngelosVip/src/main/java/com/AV/AngelosVip/AngelosVipApplication.java
@@ -0,0 +1,13 @@
+package com.AV.AngelosVip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelosVipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelosVipApplication.class, args);
+	}
+
+}
---
+++ proyecto-java/AngelosVip/src/test/java/com/AV/AngelosVip/AngelosVipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AV.AngelosVip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelosVipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
