e1fcf475330aaea86e2152602f9844d0e5c9e040
yulieth07 <yulieth_junco@soy.sena.edu.co>
2025-12-08 01:25:20 -0500

    add

--- proyecto-java/AngelosVip/src/main/java/com/AV/AngelosVip/repositories/FacturaDetalleRepositories.java
+++ proyecto-java/AngelosVip/src/main/java/com/AV/AngelosVip/repositories/FacturaDetalleRepositories.java
@@ -9,8 +9,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface FacturaDetalleRepositories extends JpaRepository<Factura, Integer> {
+
     @Query("SELECT fd FROM FacturaDetalle fd HWERE " +
      "CAST(fd.idFactDet AS string) LIKE CONCAT('%', :filtro, '%')")
-
     List<FacturaDetalle> allcampos(@Param("filtro")String filtro);
 }
